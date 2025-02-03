abd308ec64d1f92b137d4d32c402dcfc3e647025
Ilham Shahfilzuah <73428619+ilhmshfz@users.noreply.github.com>
2025-02-03 15:59:20 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -112,10 +112,10 @@ const CONFIG = {
   // ],
   educations: [
     {
-      institution: 'Universiti Teknologi Malaysia',
+      institution: 'Universiti Teknologi Malaysia (UTM)f',
       degree: 'Bachelor of Computer Science (Software Engineering) with Honours',
       // from: '2015',
-      to: '2024',
+      grad: 'Graduate 2024',
     },
     // {
     //   institution: 'Institution Name',
@@ -147,7 +147,7 @@ const CONFIG = {
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    username: '', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
