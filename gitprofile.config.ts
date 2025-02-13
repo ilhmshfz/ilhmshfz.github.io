36097e56119a0458cf46e4e8f68cafaf497b6563
Ilham Shahfilzuah <73428619+ilhmshfz@users.noreply.github.com>
2025-02-13 12:13:12 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -166,7 +166,7 @@ const CONFIG = {
   //   snippetVersion: 6,
   // },
   themeConfig: {
-    defaultTheme: 'dracula',
+    defaultTheme: 'corporate',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
